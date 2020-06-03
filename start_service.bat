1011f7f1ecf9f631259f68ee01c0b7443ba4d3fe
Crackexy <63198834+Crackexy@users.noreply.github.com>
2020-06-03 11:43:16 +0530

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -24,5 +24,5 @@ if '%errorlevel%' NEQ '0' (
     pushd "%CD%"
     CD /D "%~dp0"
 :--------------------------------------
-net stop Kigyōbot
-net start Kigyōbot
+net stop Miss CIA
+net start Miss CIA
